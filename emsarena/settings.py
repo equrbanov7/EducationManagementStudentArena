b09cb19d25181f387c74d4b31204ee9931cc02f4
Elvin <elvin@Elvins-MacBook-Pro.local>
2026-02-02 03:19:23 +0400

    Using docker yml

--- emsarena/settings.py
+++ emsarena/settings.py
@@ -110,10 +110,17 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+#         conn_max_age=600,
+#     )
+# }
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
+        conn_health_checks=True,  # Bağlantıları yoxlayır
     )
 }
 
