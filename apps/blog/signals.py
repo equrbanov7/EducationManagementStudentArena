ce89752a5982bb89fd39347cc89fb91cdbab9891
Elvin <elvingurbanov@list.ru>
2026-03-27 17:32:15 +0400

    Merged v1

--- apps/blog/signals.py
+++ apps/blog/signals.py
@@ -1,7 +1,6 @@
 import logging
 
 from django.contrib.auth import get_user_model
-from django.db.models import Q
 from django.db.models.signals import post_delete, post_save, pre_save
 from django.dispatch import receiver
 from django.urls import reverse
